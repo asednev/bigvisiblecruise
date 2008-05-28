e2b083f10d2bcaa4d196256072fca03b64264d6c
bencarey <bencarey@292e73ef-553f-0410-ac38-3dd74aee3197>
2008-05-28 20:06:00 +0000

    Started experimenting with validations.

---
+++ src/BigVisibleCruise.Tests/Converters/BooleanToVisibleConverter_Test.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using BigVisibleCruise.Converters;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace BigVisibleCruise.Tests.Converters
+{
+
+    [TestFixture]
+    public class BooleanToVisibleConverter_Test
+    {
+
+        [Test]
+        public void false_results_in_collapsed()
+        {
+            IValueConverter converter = new BooleanToVisibleConverter();
+            Assert.That(converter.Convert(false, null, null, null), Is.EqualTo(Visibility.Collapsed));
+        }
+
+        [Test]
+        public void true_results_in_visible()
+        {
+            IValueConverter converter = new BooleanToVisibleConverter();
+            Assert.That(converter.Convert(true, null, null, null), Is.EqualTo(Visibility.Visible));
+        }
+
+        [Test]
+        public void false_results_in_visible_when_parameter_is_specified()
+        {
+            IValueConverter converter = new BooleanToVisibleConverter();
+            Assert.That(converter.Convert(true, null, false, null), Is.EqualTo(Visibility.Collapsed));
+        }
+    }
+
+}
