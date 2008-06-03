08aa39a9eaa67af0ca514fc503e1b19cd320310d
bencarey <bencarey@292e73ef-553f-0410-ac38-3dd74aee3197>
2008-06-03 20:45:50 +0000

    Organized images.

--- trunk/src/BigVisibleCruise/Validations/DashboardUrlValidationRule.cs
+++ trunk/src/BigVisibleCruise/Validations/DashboardUrlValidationRule.cs
@@ -16,14 +16,9 @@ namespace BigVisibleCruise.Validations
 
             if (text == string.Empty)
             {
-                return new ValidationResult(false, "Please enter a dasbhoard url.\n\nYou can see an example at http://ccnetlive.thoughtworks.com/ccnet");
-            }
 
-//            Regex urlRegex = new Regex("(([a-zA-Z][0-9a-zA-Z+\\-\\.]*:)?/{0,2}[0-9a-zA-Z;/?:@&=+$\\.\\-_!~*'()%]+)?(#[0-9a-zA-Z;/?:@&=+$\\.\\-_!~*'()%]+)?");
-//            if (!urlRegex.IsMatch(text))
-//            {
-//                return new ValidationResult(false, "The url that you have entered does not appear to be in a valid format.\n\nYour url should be in a format similar to: http://ccnetlive.thoughtworks.com/ccnet");
-//            }
+                return new ValidationResult(false, "Please enter the url of a dasbhoard. You can see an example of a dashboard at http://ccnetlive.thoughtworks.com/ccnet");
+            }
 
             try
             {
@@ -32,7 +27,7 @@ namespace BigVisibleCruise.Validations
             }
             catch
             {
-                string errorMessage = string.Format("There was a problem trying to get information from {0}.\n\nMake sure that you can browse to this page in a browser.\n\nYou should also be using a recent release of Cruise.", text);
+                string errorMessage = string.Format("There was a problem trying to get information from {0}. Make sure that you can open that page in a browser and that is is pointing to Cruise Control dashboard.\n\nIf everything looks ok to you and the url still doesn't validate, please post a topic on www.getsatisfaction.com/bigvisiblecruise and we will do our best to track down the issue." , text);
                 return new ValidationResult(false, errorMessage);
             }
 
