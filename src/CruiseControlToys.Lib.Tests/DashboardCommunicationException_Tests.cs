5f67a98418bca4be84dea5bc67f4e275be9277bb
bencarey <bencarey@292e73ef-553f-0410-ac38-3dd74aee3197>
2008-06-19 17:39:29 +0000

    Updated to get full coverage on the lib project.

---
+++ src/CruiseControlToys.Lib.Tests/DashboardCommunicationException_Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace CruiseControlToys.Lib.Tests
+{
+
+    [TestFixture]
+    public class DashboardCommunicationException_Tests
+    {
+
+        [Test]
+        public void check_that_the_uri_is_accessible_when_the_dashboard_exception_is_thrown() 
+        {
+            DashboardCommunicationException exception = new DashboardCommunicationException(new Uri("http://www.foo.com"), null);
+            Assert.That(exception.Uri, Is.EqualTo(new Uri("http://www.foo.com")));
+        }
+
+    }
+
+}
